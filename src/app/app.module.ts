35d9a5983c0e3434b7bdb73ccc6359dbc3c3dcaa
Özgür Aslan <ozgur.aslan@defacto.com>
2021-06-13 15:28:27 +0300

    başlangıç paketleri yüklendi

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,29 @@
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
+import { CommonModule } from '@angular/common';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 
+import {IgxButtonModule} from 'igniteui-angular';
+
+// import { ToastrModule } from 'ngx-toastr';
+// import { NgxSpinnerModule } from "ngx-spinner";
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CommonModule,
+    BrowserAnimationsModule,
+    IgxButtonModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
-export class AppModule { }
+export class AppModule {
+}
