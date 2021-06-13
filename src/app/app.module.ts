a930676546b96b473b4ac877090d92c77e9669e8
Özgür Aslan <ozgur.aslan@defacto.com>
2021-06-13 15:52:08 +0300

    başlangıç componentleri oluşturuldu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,20 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 import {IgxButtonModule} from 'igniteui-angular';
+import { HomeComponent } from './home/home.component';
+import { UserComponent } from './user/user.component';
+import { RoleComponent } from './role/role.component';
+import { UserDetailComponent } from './user/user-detail/user-detail.component';
 
 // import { ToastrModule } from 'ngx-toastr';
 // import { NgxSpinnerModule } from "ngx-spinner";
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    UserComponent,
+    RoleComponent,
+    UserDetailComponent
   ],
   imports: [
     BrowserModule,
